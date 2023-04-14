bbe9ee9b679c47b33f2eea065f2b882f3f2f7887
Emon <emon.mhk69@gmail.com>
2023-04-15 01:04:19 +0600

    registration done

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { app } from '../firebase/firebase.config';
 
 const Register = () => {
+
+    const auth = getAuth(app);
+
+    const [errors , setErrors] = useState("");
+    const [success, setSuccess] = useState("");
 
     const handleSubmit =(event)=>{
         event.preventDefault();
         const email = event.target.email.value;
         const password = event.target.password.value;
+        setErrors("");
+        setSuccess("");
+
+        //validation:
+        if(!/(?=.*[A-Z])/.test(password)){
+            setErrors("Please Add Atleaset 1 Upper Case in password");
+            return;
+        }
+        else if(!/(?=.*[0-9].*[0-9])/.test(password)){
+            setErrors("Please add Atleaset 2 letter in password")
+            return;
+        }
+        else if(password.length < 6){
+            setErrors("Please atleast 6 character in password")
+            return;
+        }
+
+        createUserWithEmailAndPassword(auth,email,password)
+        .then(response=>{
+            const user = response.user;
+            setSuccess("User SuccessFully Created");
+            event.target.reset();
+        })
+        .catch(error=>{
+            console.error(error);
+            setErrors(error.message);
+        })
     }
 
     return (
@@ -13,12 +47,16 @@ const Register = () => {
             <h2>Please Register</h2>
 
             <form onSubmit={handleSubmit}>
-                <input className='w-100 mb-4 p-2 rounded' type="email" name="email" id="email" placeholder='your E-mail'/>
+                <input className='w-100 mb-4 p-2 rounded' type="email" name="email" id="email" placeholder='your e-mail' required/>
                 <br />
-                <input className='w-100 mb-4 p-2 rounded' type="password" name="password" id="password" placeholder='your password'/>
+                <input className='w-100 mb-4 p-2 rounded' type="password" name="password" id="password" placeholder='your password' required/>
                 <br />
                 <input className='btn btn-primary' type="submit" value="Register" />
             </form>
+
+            <p className='text-danger'>{errors}</p>
+            <p className='text-success'>{success}</p>
+
         </div>
     );
 };
---
+++ src/Components/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCNcxRMnW5PcWCO9F5kfuhAO0j6c3mnozc",
+  authDomain: "react-email-pass-auth-a1c66.firebaseapp.com",
+  projectId: "react-email-pass-auth-a1c66",
+  storageBucket: "react-email-pass-auth-a1c66.appspot.com",
+  messagingSenderId: "916713672939",
+  appId: "1:916713672939:web:554b22288049e8e23beeae"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
